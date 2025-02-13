305121dd742c96d0f29c815cde2bb753086c7bce
Robbie Frank <r.frank117@gmail.com>
2025-02-13 10:13:43 -0500

    add comments

--- B453 FPS Lab Activity/Assets/Scripts/Pistol.cs
+++ B453 FPS Lab Activity/Assets/Scripts/Pistol.cs
@@ -12,8 +12,12 @@ public class Pistol : Weapon
 
     protected override void Shoot()
     {
+        // Create a local variable to store raycast hit data.
         RaycastHit hit;
+        // Shoot out a raycast that's visible within the game view.
         Debug.DrawRay(firePoint.position, firePoint.forward * range, Color.red, 1f);
+        // This fires a raycast from the firepoint in the forward direction, storing the hit data in our hit variable.
+        // The length of the raycast is determined by the range of the weapon.
         if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
--- B453 FPS Lab Activity/Assets/Scripts/Weapon.cs
+++ B453 FPS Lab Activity/Assets/Scripts/Weapon.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public abstract class Weapon : MonoBehaviour
 {
+    // The location in space where the projectiles (or raycast) will be spawned.
     [SerializeField] protected Transform firePoint;
+    // How much damage this weapon does.
     [SerializeField] protected float damage;
+    // The range of this weapon.
     [SerializeField] protected float range;
+    // How quickly this weapon can fire.
     [SerializeField] protected float fireRate;
+    // How many bullets this weapon can hold.
     [SerializeField] protected float bulletCount;
 
     protected virtual void Shoot()
